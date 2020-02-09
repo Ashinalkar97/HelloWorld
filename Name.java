5d66e7861f03030eecac838e9e590139d3d9c17e
Ashinalkar97 <ashinalkar97@gmail.com>
2020-02-09 18:55:04 +0530

    MyNewFile

---
+++ Name.java
@@ -0,0 +1,9 @@
+public class Name{
+	public static void main(String[] args){
+		String fname = "Ashish";
+		System.out.println("Hello " +fname);
+		String sname = "Shinalkar";
+		String fullname = fname + sname;
+		System.out.println(fullname);
+		}
+	} 
