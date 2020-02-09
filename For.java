37a13a3a7412acba599856742b862670423add6c
Ashinalkar97 <ashinalkar97@gmail.com>
2020-02-09 19:01:40 +0530

    Added new FOR file

---
+++ For.java
@@ -0,0 +1,9 @@
+public class For{
+	public static void main(String[] args){
+		int i = 0;
+		for (i = 0; i < 6; i++)
+		{
+		System.out.println("Running Running Just to Keep My Hands On You");
+		}
+	}
+	}
